ab7e65ef1a77add9974e1b2cde12dd22d0c2b528
avoiddeveloper <karimwdd03@gmail.com>
2024-11-08 01:18:31 +0600

    navbar structure added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        'theme-primary': '#fcb414',
+      },
       fontFamily: {
         "roboto": ['Roboto', 'sans-serif'],
       },
